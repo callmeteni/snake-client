c1521c16d1aa1c4d7018f7c1fb355792127bad18
Teniola Ogunlaja <isaacogunlaja9@gmail.com>
2023-06-21 04:22:49 +0000

    sending move commands to the server

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+    connection = conn;
     const stdin = process.stdin;
     stdin.setRawMode(true);
     stdin.setEncoding("utf8");
@@ -7,11 +10,24 @@ const setupInput = function () {
     return stdin;
   };
   
-  const handleUserInput = function () {
+  const handleUserInput = function (data) {
   // Terminate the game if Ctrl+C (ASCII value '\u0003') is pressed
-  if (data === '\u0003')
+  if (data === '\u0003'){
   process.exit();
-  };
+  }
+  else if (data === 'w'){
+    connection.write("Move: up");
+  }
+  else if (data === 's'){
+    connection.write("Move: down");
+  }
+  else if (data === 'a'){
+    connection.write("Move: left");
+  }
+  else if (data === 'd'){
+    connection.write("Move: right");
+  }
+}
 
   module.exports = setupInput;
 
