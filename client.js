09b215c4a3dc7889d81d31ff6cb20023afdd8569
Teniola Ogunlaja <isaacogunlaja9@gmail.com>
2023-06-21 04:56:56 +0000

    cleaned up code

--- client.js
+++ client.js
@@ -1,44 +1,31 @@
 const { IP, PORT } = require("./constants");
-
 const net = require("net");
+
 const connect = function () {
     const conn = net.createConnection({
-      host: IP, // IP address here,
-      port: PORT, // PORT number here,
+        host: IP,
+        port: PORT,
     });
-  
-    // interpret incoming data as text
+
+    // Interpret incoming data as text
     conn.setEncoding("utf8");
-  
-   conn.on("connect", () => {
-      console.log("Connected to the server!");
-    //   conn.write("Name: TOO");
-    //   conn.write("Move: up");
-    //   conn.write("Move: down");
-    //   conn.write("Move: left");
-    //   conn.write("Move: right");
-      // Perform actions after successful connection
-  
-      // Code to handle incoming data when the connection is first established
-      conn.on("data", (data) => {
-        console.log("Server says: ", data);
-        // Perform actions with the received data
-      });
+
+    conn.on("connect", () => {
+        console.log("Connected to the server!");
+
+        // Code to handle incoming data when the connection is first established
+        conn.on("data", (data) => {
+            console.log("Server says: ", data);
+            // Perform actions with the received data
+        });
     });
 
-    // setInterval(() => {
-    //     conn.write("Move: up")
-    // }, 50);
-
-  
     conn.on("close", () => {
-      console.log("Connection closed!");
-      // Perform actions when the connection is closed
+        console.log("Connection closed!");
+        // Perform actions when the connection is closed
     });
-  
+
     return conn;
-  };
+};
 
-  
-
-  module.exports = connect;
+module.exports = connect;
