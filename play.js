96b373eacb88f542b2279bc6f3240b16c711102d
Teniola Ogunlaja <isaacogunlaja9@gmail.com>
2023-06-21 03:19:23 +0000

    refactoring add the setupInput function to input.js's module.exports object.

--- play.js
+++ play.js
@@ -1,26 +1,7 @@
-const connect = require("./client.js")
-
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function () {
-// Terminate the game if Ctrl+C (ASCII value '\u0003') is pressed
-if (data === '\u0003')
-process.exit();
-};
-
-setupInput();
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-
-
-
+setupInput();
